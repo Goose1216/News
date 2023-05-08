0b573e5f8a53235df983fc16dbadfb5695aebc1e
Goose1216 <leha.porublev@mail.ru>
2023-05-08 21:08:25 +0300

    Permissions and Authorization

--- articles/views.py
+++ articles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import DeleteView, UpdateView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from . import models
 
 
@@ -15,19 +16,26 @@ class ArticleDetailView(DetailView):
     template_name = 'article_detail.html'
 
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UpdateView):
     model = models.Article
     fields = ['title', 'body', ]
     template_name = 'article_edit.html'
+    login_url = 'login'
 
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin, DeleteView):
     model = models.Article
     template_name = 'article_delete.html'
+    login_url = 'login'
     success_url = reverse_lazy('article_list')
 
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = models.Article
     template_name = 'article_new.html'
-    fields = ['title', 'body', 'author', ]
+    fields = ['title', 'body', ]
+    login_url = 'login'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
