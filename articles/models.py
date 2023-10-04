549c2ca133cd9a5a06dbd84fdae48dad287f7b2c
Goose1216 <leha.porublev@mail.ru>
2023-10-04 15:59:28 +0300

    add slug`s

--- articles/models.py
+++ articles/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.urls import reverse
-
+from django.template.defaultfilters import slugify
 
 class Article(models.Model):
     title = models.CharField(max_length=255, unique=True)
@@ -11,6 +11,7 @@ class Article(models.Model):
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
     )
+    slug = models.SlugField(blank=True, unique=True, editable=False, default='_')
 
     class Meta:
         ordering = ['date']
@@ -19,7 +20,11 @@ class Article(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('article_detail', args=[str(self.id)])
+        return reverse('article_detail', kwargs={'slug': self.slug})
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
 
 class Comment(models.Model):
@@ -35,4 +40,4 @@ class Comment(models.Model):
         return self.comment
 
     def get_absolute_url(self):
-        return reverse('article_detail', args=[str(self.article.id)])
+        return reverse('article_detail', kwargs={'slug': self.article.slug})
--- articles/urls.py
+++ articles/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.ArticleListView.as_view(), name='article_list'),
-    path('<int:pk>/edit', views.ArticleUpdateView.as_view(), name='article_edit'),
-    path('<int:pk>/', views.ArticleDetailView.as_view(), name='article_detail'),
-    path('<int:pk>/delete', views.ArticleDeleteView.as_view(), name='article_delete'),
     path('new/', views.ArticleCreateView.as_view(), name='article_new'),
-    path('<int:pk>/comment/', views.CommentCreateView.as_view(), name='comment_new')
+    path('<slug:slug>/edit', views.ArticleUpdateView.as_view(), name='article_edit'),
+    path('<slug:slug>/', views.ArticleDetailView.as_view(), name='article_detail'),
+    path('<slug:slug>/delete', views.ArticleDeleteView.as_view(), name='article_delete'),
+    path('<slug:slug>/comment/', views.CommentCreateView.as_view(), name='comment_new')
 ]
